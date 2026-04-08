ec1fd1f6d8f85b392c5700618b07dd9c68644ea1
Tanvir Kaur Bedi <93431883+TanvirKaurBedi@users.noreply.github.com>
2026-04-08 22:34:53 +0530

    made feed api for loggedIn User

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequests = require("../models/connectionRequest");
+const { connect, connections } = require("mongoose");
+const User = require("../models/user");
 
 // get all the pending connection requests for the loggedIn User
 router.get("/user/requests/received", userAuth, async (req, res) => {
@@ -11,6 +13,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
       toUserId: loggedInUser._id,
       status: "interested",
     }).populate("fromUserId", ["name", "email"]);
+
     res.json({ message: "Data Fetched Succesfully", data: connectionRequests });
   } catch (err) {
     console.log(err);
@@ -27,8 +30,64 @@ router.get("/user/connections", userAuth, async (req, res) => {
         { fromUserId: loggedInUser._id, status: "accepted" },
         { toUserId: loggedInUser._id, status: "accepted" },
       ],
-    }).populate("fromUserId toUserId", ["name", "email"]);
-    res.json({ message: "Data Fetched Successfully", data: connections });
+    })
+      .populate("fromUserId", ["name", "email"])
+      .populate("toUserId", ["name", "email"]);
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ message: "Data Fetched Successfully", data: data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+// get all the feed
+router.get("/feed", userAuth, async (req, res) => {
+  try {
+    // User shoul see all the cards except :
+    // his own Card
+    // his connections cards
+    // Card of user whom he has rejected or ignored
+    // Card of user who has rejected or ignored him
+    // Card of user who has sent him request but he has not responded yet
+    // Card of user to whom he has sent request but the user has not responded yet
+
+    const loggedInUser = req.user;
+
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const connectionRequests = await ConnectionRequests.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId status");
+
+    const hiddenUserFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hiddenUserFromFeed.add(req.fromUserId.toString());
+      hiddenUserFromFeed.add(req.toUserId.toString());
+    });
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hiddenUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .skip(skip)
+      .limit(limit);
+
+    res.send({
+      message: "Data Fetched Successfully",
+      data: users,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Internal Server Error" });
