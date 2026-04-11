b78a4bd910c30e3613020be62156ab97dfeeae0d
Tanvir Kaur Bedi <93431883+TanvirKaurBedi@users.noreply.github.com>
2026-04-11 12:47:16 +0530

    added header and footer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+
 function App() {
   return (
     <>
-      <p>Hello World</p>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Body />
+              </div>
+            }
+          >
+            <Route path="" element={<div></div>} />
+            <Route path="" element={<div></div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
